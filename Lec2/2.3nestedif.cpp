6f0bb44c20041dc49710a80445b0edd845da7856
Dharmik1 <dharmikumar.patel@strathmore.edu>
2016-04-19 13:18:54 +0300

    assignment

---
+++ Lec2/2.3nestedif.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x =6;
+    int y =0;
+    
+    if(x>y)
+    {
+        cout<< "x is greater than y"<<endl;
+    if(x==6)
+        cout<<"x is equal to 6"<<endl;
+        else 
+        cout<< "x is not equal to 6"<<endl;
+    }
+    else
+        cout <<"x is not greater than y"<<endl;
+    return 0;
+}
+    
